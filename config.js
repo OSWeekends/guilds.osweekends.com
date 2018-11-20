376f0843c639c46650856ef98e1b70319a47fd7f
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2018-11-20 20:35:10 +0000

    Added Passport and migrated to ejs from pug

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
 const config = {
-    port: process.env.PORT || 3000
-}
+    port: process.env.PORT || 3000,
+    session: {
+        secret: process.env.SESSION_SECRET || "",
+        resave: false,
+        saveUninitialized: false
+    },
+    github: {
+        clientID: process.env.GITHUB_CLIENT_ID || "",
+        clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+        callback: process.env.GITHUB_CALLBACK || ""
+    }
+};
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express'),
     helmet = require('helmet'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    partials = require('express-partials'),
     config = require('./config'),
-    logger = require('./logger');
+    logger = require('./lib/logger'),
+    auth = require('./lib/auth');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 // Middelware
 app.use(helmet());
@@ -11,15 +17,57 @@ app.use((req, res, next) => {
     logger.info(`[${new Date().getTime()}] | IP: ${req.connection.remoteAddress} | UserAgent: ${req.headers['user-agent']}`);
     next();
 });
-
-app.set('view engine', 'pug');
+app.use(partials());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(session(config.session));
+app.use(auth.passport.initialize());
+app.use(auth.passport.session());
+app.use(express.static(__dirname + '/public'));
 
 // Routes
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome to Guilds.osweekends.com', message: '👋 Guilder!'});
+    res.render('index', {
+        title: 'Welcome to Guilds.osweekends.com',
+        message: '👋 Guilder!',
+        user: req.user
+    });
 });
+
+app.get('/profile', auth.ensureAuthenticated, (req, res) => {
+    res.render('profile', {
+        user: req.user,
+        data: req.user._json
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {
+        user: req.user
+    });
+});
+
+app.get('/auth/github', auth.passport.authenticate('github', {
+    scope: ['user:email']
+}));
+
+app.get('/auth/github/callback',
+    auth.passport.authenticate('github', {
+        failureRedirect: '/login'
+    }), (req, res) => {
+        res.redirect('/profile');
+    });
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
 
 // Port
 app.listen(config.port, () => {
-  logger.info(`[INFO] Server listening on port ${config.port}!`);
+    logger.info(`[INFO] Server listening on port ${config.port}!`);
 });
