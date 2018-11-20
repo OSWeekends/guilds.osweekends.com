b5abe706fc6df1641333dbb14419b1355555edfe
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2018-11-20 16:08:55 +0000

    Added Helmet to the projet

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express'),
+    helmet = require('helmet'),
     config = require('./config'),
     logger = require('./logger');
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello Guilders!');
